d204840a9482e62973e445f45a880c267912a61e
ks <zavadolks@gmail.com>
2022-10-10 16:28:22 +0300

    constants

---
+++ constants.js
@@ -0,0 +1,29 @@
+export const COLORS = {
+    primary: '#fff',
+    secondary: '#1FECFC',
+
+    headerBackground: '#1E2032',
+    cardBackground: '#05071B',
+    background: '#05071B',
+    ptimaryBlue: '#1FECFC',
+    basicText: '#FFFFFF',
+    lightText: '#CDCDD1',
+}
+
+export const SIIZES= {
+    padding: 12,
+    font: 14,
+    radius:8,
+
+    //font sizes
+    h1:24,
+    h2:22,
+    h3:16,
+    h4:14,
+    body1:16,
+    body2:14,
+    body3:10
+
+}
+const appTheme = {COLORS, SIIZES}
+export default appTheme
